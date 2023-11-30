5a83ab6d57f37a4404d0ee99d3d3858816ba238e
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2023-11-29 21:20:06 -0500

    Actualizacion 0.2

--- PYICICLO4/resources/views/livewire/prueba.blade.php
+++
@@ -1,5 +0,0 @@
-<body class="bg-gray-200 flex justify-center items-center h-screen">
-    <div class="w-64 h-64 bg-blue-200 flex items-center justify-center">
-      <div class="w-16 h-16 bg-red-500"></div>
-    </div>
-  </body>
